e2a6c7d519c1261ab35071eccd906f7b22b64a89
Allen Rose <RoseAllenM@gmail.com>
2026-03-11 21:45:56 -0700

    Convert CLI to typer

--- python/{{cookiecutter.package_slug}}/src/{{cookiecutter.package_slug}}/_cli.py
+++ python/{{cookiecutter.package_slug}}/src/{{cookiecutter.package_slug}}/_cli.py
@@ -1,49 +1,49 @@
 # TODO Remove this file if not in use.
 
-import argparse
 import logging
-from typing import Optional
+
+from enum import Enum
+
+import typer
 
 logger = logging.getLogger(__name__)
 
 
-def _parse_arguments(args_list: Optional[list[str]]=None) -> argparse.Namespace:
-    """Create an ArgParser and parse the given argument values."""
-    parser = argparse.ArgumentParser(
-        description="TODO Summary of what this command does.",
-    )
-
-    # TODO Add any arguments necessary for your command.
-
-    verbosity = parser.add_mutually_exclusive_group()
-    verbosity.add_argument(
-        "--info",
-        help="Set logging level to INFO.",
-        action="store_const",
-        const=logging.INFO,
-        dest="logging_level",
-    )
-    verbosity.add_argument(
-        "--debug",
-        help="Set logging level to DEBUG.",
-        action="store_const",
-        const=logging.DEBUG,
-        dest="logging_level",
-    )
-
-    args = parser.parse_args(args_list)
-    # args, extra_args = parser.parse_known_args(args_list)
-
-    logging.basicConfig(level=args.logging_level)
-    logger.debug("Parsed Arguments:\n\t%s", vars(args))
-
-    return args
+class _LogLevel(str, Enum):
+    debug = "debug"
+    info = "info"
+    warning = "warning"
+    error = "error"
 
 
-def run_example(args_list: Optional[list[str]]=None):
-    """Execute the 'example' command."""
-    args = _parse_arguments(args_list=args_list)
+app = typer.Typer(
+    help="TODO Summary of what this command does.",
+    add_completion=False,
+    context_settings={"help_option_names": ["-h", "--help"]},
+    no_args_is_help=True,
+)
 
-    # TODO Call whatever function(s) with the argument values from the args namespace.
+@app.callback()
+def set_logging(log_level: _LogLevel = _LogLevel.warning):
+    """Set the desired logging level for any command."""
+    logging.basicConfig(level=getattr(logging, log_level.value.upper()))
 
-    print(args)  # TODO Remove this line.
+
+@app.command()
+def jump_up(
+    height: int,
+) -> None:
+    """TODO action that this CLI command will do."""
+    # TODO Call whatever function(s) with the option values.
+
+    typer.echo("Complete")  # TODO Replace with argument output.
+
+
+@app.command()
+def get_down(
+    height: str = "ground",
+) -> None:
+    """TODO action that this CLI command will do."""
+    # TODO Call whatever function(s) with the argument values.
+
+    typer.echo("Complete")  # TODO Replace with informative output.
